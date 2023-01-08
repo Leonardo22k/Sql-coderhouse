4f5ca8305d33f91d8dfb09e6f1beda3a81f6a994
Leo <73497957+Leonardo22k@users.noreply.github.com>
2023-01-08 20:25:21 -0300

    Update CasaDeComputacion Proyecto.sql

--- CasaDeComputacion Proyecto.sql
+++ CasaDeComputacion Proyecto.sql
@@ -272,12 +272,3 @@ CREATE OR REPLACE VIEW Total_productos_en_almacen
 AS
 SELECT sum(cantidad_producto) AS cantidad_de_productos FROM almacen;
 
-                           -- CREACIÓN DE USUARIOS CON PRIVILEGIOS -- 
-
--- Este usuario solo podra ver las tablas y seleccionarlas -- 
-CREATE USER 'usuariodeprueba1@coder' IDENTIFIED WITH mysql_native_password BY '12345';
-	GRANT SELECT ON *.* TO 'usuariodeprueba1@coder';
-
--- Este usuario podra ver las tablas, insertar datos y modificar -- 
-CREATE USER 'usuariodeprueba2@coder' IDENTIFIED WITH mysql_native_password BY '12345';
-	GRANT SELECT, INSERT, UPDATE ON *.* TO 'usuariodeprueba2@coder';
